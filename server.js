e7e3d87302406693e62c9026c089499732b184e2
Kellen <kellenag115@gmail.com>
2025-07-25 13:30:43 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
